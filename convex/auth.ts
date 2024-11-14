b0e56410219824d777b06e2e01404bec6a0b0aa4
Igor Silva <igor@igorsilva.pro>
2024-11-14 17:25:32 +0000

    adding some comments

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,6 +1,30 @@
 import Google from '@auth/core/providers/google';
 import { convexAuth } from '@convex-dev/auth/server';
 
+// TODO: limit to my domains only
+// that's how I used to do with Auth.js on Next
+// 		const allowedDomains = ['igorsilva.pro'];
+// 		const allowedUsers = ['igor9ferreira9@gmail.com'];
+
+// 		async createOrUpdateUser(ctx, args) {
+// 			const emails = args.profile.email;
+
+// 			if (!email) return false;
+// 			if (!email_verified) return false;
+
+// 			if (allowedUsers.includes(email)) {
+// 				return true;
+// 			}
+
+// 			if (allowedDomains.some((domain) => email?.endsWith(domain))) {
+// 				return true;
+// 			}
+
+// 			return false;
+// 		},
+// it doesnt seem to be possible to do this with Convex Auth
+// at their discord someone suggested to solve this with authorization, which is not ideal
+
 export const { auth, signIn, signOut, store } = convexAuth({
 	providers: [Google],
 	callbacks: {
@@ -8,5 +32,4 @@ export const { auth, signIn, signOut, store } = convexAuth({
 			console.log('afterUserCreatedOrUpdated', ctx, args);
 		},
 	},
-	// TODO: limit to my domains only
 });
