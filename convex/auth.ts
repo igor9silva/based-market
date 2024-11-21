5e009e0656110122971071281e34de6caeb0fd60
Igor Silva <igor@igorsilva.pro>
2024-11-21 13:46:15 +0000

    increasing JWT session duration (ideally should auto re-auth)

--- convex/auth.ts
+++ convex/auth.ts
@@ -27,6 +27,9 @@ import { convexAuth } from '@convex-dev/auth/server';
 
 export const { auth, signIn, signOut, store } = convexAuth({
 	providers: [Google],
+	jwt: {
+		durationMs: Number(process.env.JWT_SESSION_DURATION_MS || 60 * 60 * 24 * 7 /* 7 days */), // TODO: use typed env
+	},
 	callbacks: {
 		async afterUserCreatedOrUpdated(ctx, args) {
 			console.log('afterUserCreatedOrUpdated', ctx, args);
