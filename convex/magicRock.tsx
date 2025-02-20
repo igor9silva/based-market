938d10ee8eb70a2a7464d1fdbc994f96a6ce5493
Igor Silva <igor@igorsilva.pro>
2025-02-20 18:57:51 +0000

    Uncomment user information and update history validation comments

--- convex/magicRock.tsx
+++ convex/magicRock.tsx
@@ -32,16 +32,16 @@ export async function _askMagicRock(
 			instructions,
 			//
 			// TODO: dynamic user info based on Inbox
-			// `## User information`,
-			// `- Language: I speak English (advanced, preferred), Portuguese (native) and a little bit of Spanish.`,
-			// `- Location: I live in Setúbal, Portugal.`,
-			// `- Timezone: UTC.`, // TODO: dynamic timezone because of the damn DST
-			// `- Current time: ${new Date().toISOString()}`,
-			// `- Name: Igor Silva`,
-			// `- Twitter: @igor9silva`,
-			// `- Birthday: 1997-01-22 (aged 27 as of today)`,
-			// `- He is your creator. He's actively working on improving you (Meseeks, the app).`,
-			// ``,
+			`## User information`,
+			`- Language: I speak English (advanced, preferred), Portuguese (native) and a little bit of Spanish.`,
+			`- Location: I live in Setúbal, Portugal.`,
+			`- Timezone: UTC.`, // TODO: dynamic timezone because of the damn DST
+			`- Current time: ${new Date().toISOString()}`,
+			`- Name: Igor Silva`,
+			`- Twitter: @igor9silva`,
+			`- Birthday: 1997-01-22 (aged 27 as of today)`,
+			`- He is your creator. He's actively working on improving you (Meseeks, the app).`,
+			``,
 			//
 			`## The task`,
 			promptForTask(task),
@@ -175,9 +175,9 @@ async function renderHistory(
 
 	console.debug('renderHistory', history);
 
-	// validateHistory(history);
+	// validateHistory(history); we're now validating before add react() action, TODO: revist this
 
-	console.debug('renderHistory validated');
+	// console.debug('renderHistory validated');
 
 	return history;
 }
