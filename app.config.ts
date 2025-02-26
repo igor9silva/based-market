aa1b6746364ba8614b9fb56273724bb887cc2311
Igor Silva <igor@igorsilva.pro>
2025-02-26 19:49:06 +0000

    untest

--- app.config.ts
+++ app.config.ts
@@ -4,9 +4,6 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	vite: {
-		build: {
-			target: 'es2022',
-		},
 		plugins: [
 			tsConfigPaths({
 				projects: ['./tsconfig.json'],
