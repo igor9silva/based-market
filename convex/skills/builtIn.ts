8341350fc6d128dfd93bbf9740e3aac68ca1fc90
Igor Silva <igor@igorsilva.pro>
2025-02-28 15:21:43 +0000

    added reasoning

--- convex/skills/builtIn.ts
+++ convex/skills/builtIn.ts
@@ -33,6 +33,16 @@ export const _builtInSkills = {
 		}),
 		execute: (execution: ToolExecution) => (args) => Promise.resolve(args.message),
 	}),
+	reason: defineSkill({
+		isVisibleToMagicRock: true,
+		preApprovedCost: 0n,
+		description:
+			'Use this tool to reason about your next decision. Feel free to use it as many times as needed. Nothing you say here will be visible to the user, but will be visible to your next iterations. Note: reasoning before making a decision increases the quality of your decisions.',
+		parameters: z.object({
+			reasoning: z.string().describe('The reasoning.'),
+		}),
+		execute: (execution: ToolExecution) => (args) => Promise.resolve(args.reasoning),
+	}),
 	increaseBudget: defineSkill({
 		isVisibleToMagicRock: false,
 		preApprovedCost: 'none',
