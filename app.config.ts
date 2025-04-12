a01a941f2abd182521fe35d69e8282734ced153a
Igor Silva <igor@igorsilva.pro>
2025-04-12 10:47:44 +0100

    added sentry

--- app.config.ts
+++ app.config.ts
@@ -1,14 +1,22 @@
 import mdx from '@mdx-js/rollup';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import { defineConfig } from '@tanstack/start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
 	vite: {
+		build: {
+			sourcemap: true,
+		},
 		plugins: [
 			tsConfigPaths({
 				projects: ['./tsconfig.json'],
 			}),
 			mdx(),
+			sentryVitePlugin({
+				org: 'ispro',
+				project: 'javascript-react',
+			}),
 		],
 	},
 	tsr: {
