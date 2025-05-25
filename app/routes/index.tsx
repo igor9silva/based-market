df512202b2908603e55e22cdc188f6b05d1c1a6b
Igor Silva <igor@igorsilva.pro>
2025-05-25 16:33:38 +0100

    added Orbital Flux

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
+import OrbitalFlux from '~/components/games/OrbitalFlux';
 
 export const Route = createFileRoute('/')({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/"!</div>;
+	return <OrbitalFlux />;
 }
